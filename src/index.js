f90f8e60904454b76be1722b254b2afc3f1300d3
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2024-12-19 08:37:27 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function generatePoem(event) {
   event.preventDefault();
 
   new Typewriter("#poem", {
-    strings: "Amanecia en el naranjel",
+    strings: "La tombe dit à la rose",
     autoStart: true,
     delay: 1,
     cursor: "",
