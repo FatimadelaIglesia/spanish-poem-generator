aa6a360bf8e23093d1a851c2d8e83563b06c5daa
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2024-12-19 11:35:21 +0000

    updated js

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "0d877aa762b4511ceo3e2a4a1bta01f6";
-  let prompt = `User instructions:Generate a Spanish poem about ${instructionsInput.value}`;
   let context =
     "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user's instructions";
+  let prompt = `User instructions:Generate a Spanish poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key} `;
   axios.get(apiUrl).then(displayPoem);
 }
